234ed681e9bd9e6d360839702c3b760a3afbe4b6
tokuicchi <tokuicchi@gmail.com>
2019-12-01 14:46:53 +0900

    propを使って実践

--- src/App.js
+++ src/App.js
@@ -14,18 +14,28 @@ import React from 'react';
 // }
 
 const App =() => {
-    return (
-        <div>
-            <Cat />
-            <Cat />
-            <Cat />
-            <Cat />
-        </div>
-    )
+  const profiles = [
+    { name:"Taro", age: 10 },
+    { name:"Hanako", age: 5 },
+    { name:"NoOne"},
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+        }
+    </div>
+  )
 }
 
-const Cat =() => {
-    return <div>Meow!!</div>
+const User =(props) => {
+  return <div>Hi, I am {props.name}, {props.age} years old! </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
